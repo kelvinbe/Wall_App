94d90a0bfaaaff5ee17190718983c65828432265
kelvin beno <kelvinbeno526@gmail.com>
2020-04-18 15:22:05 +0300

    added connection to postgresql

--- wall/views.py
+++ wall/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from rest_framework import viewsets
 from django.core.mail import send_mail
 from rest_framework.reverse import reverse
@@ -7,7 +6,7 @@ from rest_framework.response import Response
 from django.contrib.auth import authenticate
 from wall.models import Message
 from walltime.settings import EMAIL_HOST, EMAIL_PORT, EMAIL_HOST_USER, EMAIL_HOST_PASSWORD
-from wall.serializers import MessageSerializer, RegisterSerializer, User,LoginSerializer
+from wall.serializers import MessageSerializer, RegisterSerializer, User, LoginSerializer
 from rest_framework import renderers
 from rest_framework.decorators import api_view
 from wall.permissions import IsOwnerOrReadOnly
@@ -21,32 +20,31 @@ from knox.models import AuthToken
 from wall.serializers import UserSerializer
 
 
-
 class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all().order_by('-posted')
     serializer_class = MessageSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly,
                           IsOwnerOrReadOnly]
-                          
+
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
         message_list = MessageViewSet.as_view({
-        'get': 'list',
-        'post': 'create'
+            'get': 'list',
+            'post': 'create'
         })
         message_detail = MessageViewSet.as_view({
-        'get': 'retrieve',
-        'put': 'update',
-        'patch': 'partial_update',
-        'delete': 'destroy'
+            'get': 'retrieve',
+            'put': 'update',
+            'patch': 'partial_update',
+            'delete': 'destroy'
         })
+
 
 @api_view(['GET'])
 def api_root(request, format=None):
     return Response({
         'message': reverse('message-list', request=request, format=format)
     })
-
 
 
 class UserList(generics.ListAPIView):
@@ -61,29 +59,30 @@ class UserDetail(generics.RetrieveAPIView):
 
 class Regsitration(generics.GenericAPIView):
     serializer_class = RegisterSerializer
+
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception = True)
+        serializer.is_valid(raise_exception=True)
         user = serializer.save()
-        send_mail(subject= "Welcome to wall",
-        message="We welcome you to the wall application",
-        from_email=EMAIL_HOST_USER, recipient_list=[user.email],
-        fail_silently=False)
+        send_mail(subject="Welcome to wall",
+                  message="We welcome you to the wall application",
+                  from_email=EMAIL_HOST_USER, recipient_list=[user.email],
+                  fail_silently=False)
         return Response({
-        "user": RegisterSerializer(user, context = self.get_serializer_context()).data,
-        "token": AuthToken.objects.create(user)[1]
+            "user": RegisterSerializer(user, context=self.get_serializer_context()).data,
+            "token": AuthToken.objects.create(user)[1]
         })
 
 
 class Login(generics.GenericAPIView):
-        serializer_class = LoginSerializer
-        def post(self, request, *args, **kwargs):
-            serializer = self.get_serializer(data=request.data)
-            serializer.is_valid(raise_exception = True)
-            user = serializer.validated_data
-            return Response({
-            "user": RegisterSerializer(user, context = self.get_serializer_context()).data,
-            "token": AuthToken.objects.create(user)[1]
-            })
+    serializer_class = LoginSerializer
 
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data
+        return Response({
+            "user": RegisterSerializer(user, context=self.get_serializer_context()).data,
+            "token": AuthToken.objects.create(user)[1]
+        })
 
